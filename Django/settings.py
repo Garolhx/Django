9c05593c2726d9067389daaf8ff97efea064af4d
Garolhx <1287782404@qq.com>
2020-07-09 15:59:35 +0800

    ALLOWED_HOSTS = ['*']

--- Django/settings.py
+++ Django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$@6vmkiaoex&27%wd@p7)0(&=$%wxiue0qe$9ct4ih2wj2@!_l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
