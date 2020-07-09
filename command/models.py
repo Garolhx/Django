df8853f59950837ff4e44049cf29d9677ab44d70
Garolhx <1287782404@qq.com>
2020-07-09 15:23:42 +0800

    django

---
+++ command/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+# orm对应数据库的类
+
+
+class Command(models.Model):
+    pass
---
+++ command/views.py
@@ -0,0 +1,34 @@
+import os
+
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+
+#视图
+#视图处理函数
+# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.parsers import JSONParser
+
+@csrf_exempt
+def run_job(request):
+    # 判断请求头是否为json
+    if request.content_type != 'application/json':
+        # 如果不是的话，返回405
+        return HttpResponse('only support json data', status=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+    # 判断是否为post 请求
+    if request.method == 'POST':
+        try:
+            # 解析请求的json格式入参
+            data = JSONParser().parse(request)
+        except Exception as why:
+            print(why.args)
+        else:
+            print(data)
+            result = os.popen(data['command'])
+            out = result.read()
+            print(out)
+            content = {'msg': 'SUCCESS', 'context': out}
+            # 返回自定义请求内容content,200状态码
+            return JsonResponse(data=content, status=status.HTTP_200_OK)
+    # 如果不是post 请求返回不支持的请求方法
+    return HttpResponseNotAllowed(permitted_methods=['POST'])
